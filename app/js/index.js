052110f87927c59de1999cbf3c70ad6aaa4b0855
unknown <conton.jerome@yahoo.fr>
2023-03-05 22:53:25 +0100

    Ajout de touches directionnelles

--- app/js/index.js
+++ app/js/index.js
@@ -1,30 +1,117 @@
 const canvas = document.querySelector('canvas');
-
 const context = canvas.getContext('2d');
 
 canvas.width = 1024;
 canvas.height = 576;
 
-context.fillStyle = 'white'
+context.fillStyle = 'white';
 context.fillRect(0, 0, canvas.width, canvas.height);
 
-const image = new Image();
-image.src = 'app/images/PortfolioGameMap.png';
+const carteImage = new Image();
+carteImage.src = 'app/images/PortfolioGameMap.png';
 
 const joueurDownImage = new Image();
-joueurDownImage.src = 'app/images/JoueurDown.png';
+joueurDownImage.src ='app/images/JoueurDown.png';
 
-image.onload = () => {
-  context.drawImage(image, -65, -670);
-  context.drawImage(joueurDownImage,
+class BougerJoueur {
+  constructor({ position, velocity, carteImage }) {
+    this.position = position;
+    this.carteImage = carteImage;
+  };
+
+  draw() {
+    context.drawImage(this.carteImage, this.position.x, this.position.y);
+  };
+};
+
+const background = new BougerJoueur({
+  position : {x: -65, y: -670},
+  carteImage: carteImage,
+});
+
+const touche = {
+  z: {
+    press: false,
+  },
+  q: {
+    press: false,
+  },
+  s: {
+    press: false,
+  },
+  d: {
+    press: false,
+  },
+};
+
+function animationJoueur() {
+  window.requestAnimationFrame(animationJoueur);
+  background.draw();
+  context.drawImage(
+    joueurDownImage,
     0,
     0,
     joueurDownImage.width / 4,
     joueurDownImage.height,
-    canvas.width / 2 - (joueurDownImage.width / 4) / 2, 
+    canvas.width / 2 - joueurDownImage.width / 4 / 2,
     canvas.height / 2 - joueurDownImage.height / 2,
     joueurDownImage.width / 4,
     joueurDownImage.height
   );
+
+
+  if (touche.z.press && derniereTouche === 'z') {
+    background.position.y = background.position.y + 3;
+  }
+  else if (touche.q.press && derniereTouche === 'q') {
+    background.position.x = background.position.x + 3;
+  }
+  else if (touche.d.press && derniereTouche === 'd') {
+    background.position.x = background.position.x - 3;
+  }
+  else if (touche.s.press && derniereTouche === 's') {
+    background.position.y = background.position.y - 3
+  };
 };
-console.log(joueurDownImage)
+animationJoueur();
+
+let derniereTouche = '';
+window.addEventListener('keydown', (even) => {
+  switch (even.key) {
+    case 'z':
+      touche.z.press = true;
+      derniereTouche = 'z';
+      break;
+    case 'q':
+      touche.q.press = true;
+      derniereTouche = 'q';
+      break;
+
+    case 's':
+      touche.s.press = true;
+      derniereTouche = 's';
+      break;
+
+    case 'd':
+      touche.d.press = true;
+      derniereTouche = 'd';
+      break;
+  };
+});
+
+window.addEventListener('keyup', (even) => {
+  switch (even.key) {
+    case 'z':
+      touche.z.press = false;
+      break;
+    case 'q':
+      touche.q.press = false;
+      break;
+    case 's':
+      touche.s.press = false;
+      break;
+    case 'd':
+      touche.d.press = false;
+      break;
+  };
+});
