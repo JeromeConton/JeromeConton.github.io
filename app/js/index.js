44d784e29a5a71314e0c55a982eb8983f913a9e2
unknown <conton.jerome@yahoo.fr>
2023-12-06 14:57:10 +0100

    mise en place chargement boulangerie lafabrik dans 2eme maison et retour acceuil dans maison depart

--- app/js/index.js
+++ app/js/index.js
@@ -19,6 +19,16 @@ for (let i = 0; i < app1Json.length; i += 70) {
   app1Map.push(app1Json.slice(i, 70 + i));
 };
 
+const app2Map = []
+for (let i = 0; i < app2Json.length; i += 70) {
+  app2Map.push(app2Json.slice(i, 70 + i));
+};
+
+const acceuilMap = []
+for (let i = 0; i < acceuilJson.length; i += 70) {
+  acceuilMap.push(acceuilJson.slice(i, 70 + i));
+};
+
 const frontieres = [];
 const compense = {
   x:-65,
@@ -57,6 +67,34 @@ app1Map.forEach((row, i) => {
   row.forEach((symbol, j) => {
     if (symbol === 1134)
     app1.push(new Frontiere({ 
+      position: {
+        x: j * Frontiere.width + compense.x,
+        y: i * Frontiere.height + compense.y
+      },
+    }));
+  });
+});
+
+const app2 = [];
+
+app2Map.forEach((row, i) => {
+  row.forEach((symbol, j) => {
+    if (symbol === 1334)
+    app2.push(new Frontiere({ 
+      position: {
+        x: j * Frontiere.width + compense.x,
+        y: i * Frontiere.height + compense.y
+      },
+    }));
+  });
+});
+
+const acceuil = [];
+
+acceuilMap.forEach((row, i) => {
+  row.forEach((symbol, j) => {
+    if (symbol === 1334)
+    acceuil.push(new Frontiere({ 
       position: {
         x: j * Frontiere.width + compense.x,
         y: i * Frontiere.height + compense.y
@@ -134,7 +172,7 @@ const touche = {
   }
 };
 
-const bouger = [background, ...frontieres, arrierePlan, ...contact, ...app1];
+const bouger = [background, ...frontieres, arrierePlan, ...contact, ...app1, ...app2, ...acceuil];
 
 function limiteCollision({limite1, limite2}) {
   return (
@@ -149,6 +187,14 @@ const cv ={
 };
 
 const site1={
+  chargement: false
+};
+
+const site2={
+  chargement: false
+};
+
+const retourAcceuil={
   chargement: false
 };
 
@@ -169,6 +215,14 @@ function animationJoueur() {
     app1Site.draw();
   })
 
+  app2.forEach(app2Site => {
+    app2Site.draw();
+  })
+
+  acceuil.forEach(acceuilRetour => {
+    acceuilRetour.draw();
+  })
+
   joueur.draw();
   arrierePlan.draw();
   
@@ -179,6 +233,10 @@ function animationJoueur() {
   if(cv.chargement) return;
 
   if(site1.chargement) return;
+
+  if(site2.chargement) return;
+
+  if(retourAcceuil.chargement) return;
 
 //  ----------  Activation de la zone de contact pour CV----------  //
   if (touche.z.press || touche.s.press || touche.q.press || touche.d.press) {
@@ -271,6 +329,110 @@ if (touche.z.press || touche.s.press || touche.q.press || touche.d.press) {
               // activation animation Application 1
               animationCv()
               window.open("https://mypartneroutdoor.fr/");  
+              gsap.to("#chevauchement", {
+                opacity: 0,
+                duration: 0.4,
+              })
+            }
+          })
+        }
+      })
+      break;
+    }
+  };
+} 
+
+//  ----------  Activation de la zone de contact pour Application 2 ----------  //
+if (touche.z.press || touche.s.press || touche.q.press || touche.d.press) {
+  for (let i = 0; i < app2.length; i++) {
+    const app2Site = app2[i];
+    const aireChevauchement = 
+      (Math.min(joueur.position.x + joueur.width, app2Site.position.x + app2Site.width) 
+      -
+      Math.max(joueur.position.x, app2Site.position.x))
+      *
+      (Math.min(joueur.position.y + joueur.height, app2Site.position.y + app2Site.height)
+      -
+      Math.max(joueur.position.y, app2Site.position.y))
+    if (
+      limiteCollision({
+        limite1: joueur,
+        limite2: app2Site
+      }) &&
+      aireChevauchement > (joueur.width * joueur.height) /3 && Math.random() < 0.06
+    )
+      {
+      console.log("chargement app2");
+
+      // desactivation boucle animation Application 2
+      window.cancelAnimationFrame(animationId)
+
+      cv.chargement = true
+      gsap.to("#chevauchement", {
+        opacity: 1,
+        repeat: 3,
+        yoyo: true,
+        duration: 0.4,
+        onComplete() {
+          gsap.to("#chevauchement", {
+            opacity: 1,
+            duration: 0.4,
+            onComplete() {
+              // activation animation Application 2
+              animationCv()
+              window.open("https://boulangerie-lafabrik.fr/");  
+              gsap.to("#chevauchement", {
+                opacity: 0,
+                duration: 0.4,
+              })
+            }
+          })
+        }
+      })
+      break;
+    }
+  };
+} 
+
+//  ----------  Activation de la zone de contact pour retour acceuil ----------  //
+if (touche.z.press || touche.s.press || touche.q.press || touche.d.press) {
+  for (let i = 0; i < acceuil.length; i++) {
+    const acceuilRetour = acceuil[i];
+    const aireChevauchement = 
+      (Math.min(joueur.position.x + joueur.width, acceuilRetour.position.x + acceuilRetour.width) 
+      -
+      Math.max(joueur.position.x, acceuilRetour.position.x))
+      *
+      (Math.min(joueur.position.y + joueur.height, acceuilRetour.position.y + acceuilRetour.height)
+      -
+      Math.max(joueur.position.y, acceuilRetour.position.y))
+    if (
+      limiteCollision({
+        limite1: joueur,
+        limite2: acceuilRetour
+      }) &&
+      aireChevauchement > (joueur.width * joueur.height) /3 && Math.random() < 0.06
+    )
+      {
+      console.log("chargement app1");
+
+      // desactivation boucle animation retour acceuil
+      window.cancelAnimationFrame(animationId)
+
+      cv.chargement = true
+      gsap.to("#chevauchement", {
+        opacity: 1,
+        repeat: 3,
+        yoyo: true,
+        duration: 0.4,
+        onComplete() {
+          gsap.to("#chevauchement", {
+            opacity: 1,
+            duration: 0.4,
+            onComplete() {
+              // activation animation retour acceuil
+              animationCv()
+              window.open("./appCv/indexCv.html");  
               gsap.to("#chevauchement", {
                 opacity: 0,
                 duration: 0.4,
